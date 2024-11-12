3ee49284ee71293ceb4a6f85a17043fedbaf801b
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-11-12 10:35:19 +0200

    Modificate/aggiunte funzioni a form.html, script.php, risultati.php e style.css

--- risultati.php
+++ risultati.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if (!(isset($_SESSION["registrazioni"]))) {
+        $_SESSION["registrazioni"] = array(); 
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +15,39 @@
 </head>
 <body>
     <?php
+        showData();
+
+        function calculateMedia($a){
+            $somma = 0;
+            foreach ($a as $key => $value) {
+                $somma += $value;
+            }
+            return $somma / count($a);
+        }
+
+        function showData(){
+            echo "<div id='divResult'>";
+                echo "<h1>REGISTRAZIONI FATTE</h1>";
+                echo "<table>";
+                    echo "<tr>";
+                        echo "<th>Codice fiscale</th>";
+                        echo "<th>Età</th>"; 
+                    echo "</tr>";
+                        $array = $_SESSION["registrazioni"];
+                        foreach ($array as $key => $value) {
+                            echo "<tr>";
+                                echo "<td>$key</td>";
+                                echo "<td>$value</td>";
+                            echo "</tr>";
+                        }
+                echo "</table>";
+                $media = calculateMedia($array);
+                echo "<p><b><i>La media dell'età è</i></b> $media</p>";
+                echo "<br>";
+                echo "<a class='sendButton' href='./form.html'>HOME</a>";
+                
+            echo "</div>";
+        }
     ?>
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if (!(isset($_SESSION["registrazioni"]))) {
+        $_SESSION["registrazioni"] = array(); 
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +15,32 @@
 </head>
 <body>
     <?php
+        $cf = $_GET['cf'];
+        $age = $_GET['age'];
+        saveRegistration($cf, $age);
+        //var_dump($_SESSION["registrazioni"]);
+        showData($cf, $age);
+
+        function saveRegistration($cf, $a){
+            $array = $_SESSION["registrazioni"];
+            $array[$cf] = $a;
+            $_SESSION["registrazioni"] = $array;
+        }
+
+        function showData($cf, $a){
+            echo "<div id='divScript'>";
+                echo "<h1>DATI INSERITI</h1>";
+                echo "<h5>Codice fiscale inserito: </h5>" . $cf;
+                echo "<br>";
+                echo "<h5>Età inserita: </h5>" . $a;
+                echo "<br>";
+                echo "<br>";
+                echo "<a class='sendButton' href='./form.html'>HOME</a>";
+                echo "<br>";
+                echo "<br>";
+                echo "<a class='sendButton' href='./risultati.php'>MOSTRA REGISTRAZIONI</a>";
+            echo "</div>";
+        }
     ?>
 </body>
 </html>
